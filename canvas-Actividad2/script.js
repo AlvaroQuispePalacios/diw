b9ab32dcc6e0fb648c5a701d23413d0d73168bfe
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2023-12-13 16:24:48 +0100

    13-12-2023

--- canvas-Actividad2/script.js
+++ canvas-Actividad2/script.js
@@ -10,23 +10,67 @@ let json = [
   {
     product: "Footballs",
     units: 300,
-  }
+  },
 ];
 
-let canvas = document.getElementById('canvas');
+let canvas = document.getElementById("canvas");
 let ctx;
-ctx = canvas.getContext('2d');
+ctx = canvas.getContext("2d");
 
 // Units
 ctx.beginPath();
 ctx.moveTo(50, 10);
 ctx.lineTo(50, 400);
-ctx.fillText("units",25, 200);
+ctx.fillText("UNITS", 15, 200);
 ctx.stroke();
+
 
 // Products
 ctx.beginPath();
-ctx.moveTo(50,400);
-ctx.lineTo(450,400);
-ctx.fillText("products",140, 150);
+ctx.moveTo(50, 400);
+ctx.lineTo(450, 400);
+ctx.fillText("PRODUCT", 220, 450);
 ctx.stroke();
+
+
+// Products(BASKETBALL)
+ctx.beginPath();
+ctx.fillText(json[0].product.toUpperCase(), 80, 420);
+ctx.stroke();
+
+// RECTANGLE PRODUCT(BASKETBALL)
+let grdBasketball = ctx.createLinearGradient(0, 0, 200, 0);
+grdBasketball.addColorStop(0, "orange");
+grdBasketball.addColorStop(1, "white");
+ctx.fillStyle = grdBasketball;
+ctx.fillRect(70, 399, 80, -json[0].units);
+
+// Products(BASEBALL)
+ctx.fillStyle = "black";
+ctx.translate(100, 0);
+ctx.beginPath();
+ctx.fillText(json[1].product.toUpperCase(), 100, 420);
+ctx.stroke();
+
+// RECTANGLE PRODUCT(BASEBALL)
+let grdBaseball = ctx.createLinearGradient(0, 0, 200, 0);
+grdBaseball.addColorStop(0, "blue");
+grdBaseball.addColorStop(1, "white");
+ctx.fillStyle = grdBaseball;
+ctx.fillRect(90, 399, 80, -json[1].units);
+
+// Products(football)
+ctx.fillStyle = "black";
+ctx.translate(120, 0);
+ctx.beginPath();
+ctx.fillText(json[2].product.toUpperCase(), 100, 420);
+ctx.stroke();
+
+// RECTANGLE PRODUCT(BASEBALL)
+let grdFotball = ctx.createLinearGradient(0, 0, 200, 0);
+grdFotball.addColorStop(0, "red");
+grdFotball.addColorStop(1, "white");
+ctx.fillStyle = grdFotball;
+ctx.fillRect(90, 399, 80, -json[2].units);
+
+
