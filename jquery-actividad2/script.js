04ff1b9f6de8535d5fbfbb964f29a8f0b7b5a0df
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-01-11 21:16:27 +0100

    11-01-2024

--- jquery-actividad2/script.js
+++ jquery-actividad2/script.js
@@ -12,17 +12,43 @@ function generarNumeroRandom(){
 
 function generarCantidadCartas(){
     for(let i = 0; i < numeroRandom; i++){
-        $("#game").append("<div class='card'></div>");
-
+        $("#game").append(
+            `<div class='card'><span></span></div>`
+        );
     }
 }
 
 function generarContenidoEnCartas(){
-    
-}
+    let arrayDesornado = Array(numeroRandom);
 
+    // Genera el contenido del array
+    for(let i = 0; i < numeroRandom/2; i++){
+        arrayDesornado.push(i+1);
+    }
+    arrayDesornado.forEach((e) => {
+        arrayDesornado.push(e);
+    });
+
+    // Desordena el array
+    arrayDesornado = arrayDesornado.sort(() => {
+        return Math.random() - 0.5;
+    })
+
+    // Ingresa el contenido del array 
+    $(".card > span").each((index, e) => {
+        $(e).text(arrayDesornado[index]);
+        // console.log($(e).val());
+    });
+
+}
 
 
 generarNumeroRandom();
 generarCantidadCartas();
-console.log(numeroRandom);
+generarContenidoEnCartas();
+$(".card ").each((index, e) => {
+    $(e).on("click", () => {
+        console.log($(e).text());
+        // $(e).css("opacity" , 0);
+    });
+});
