2369be5c51f74e69fe9fb56e6078738b10de8371
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-01-19 17:18:46 +0100

    19-01-2024

--- jquery-actividad4/script.js
+++ jquery-actividad4/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-    $(".draggable").draggable();
+    $(".post-it-red").draggable();
+    $(".post-it-blue").draggable();
+    $("#dialog").dialog();
+
+    $("#crearPost").on("click", () => {
+        $(".post-its").append("<div class='square post-it-red'></div>");
+        $(".post-it-red").draggable();
+        $(".post-it-blue").draggable();
+    });
+
+
+    $("#tableRed").droppable({
+        accept: ".post-it-red",
+        drop: function () {
+            $(this).addClass("post-it-validate ").find("div");
+        },
+    });
+
+    $("#tableBlue").droppable({
+        accept: ".post-it-blue",
+        drop: function () {
+            $(this).addClass("post-it-validate ").find("div");
+        },
+    });
+
 });
