cc87e619ba6295db67972f8f4a1606ddcbd1414f
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-02-22 21:15:56 +0100

    22-02-2024

---
+++ vue/vue-actividad3/Mainfooter.js
@@ -0,0 +1,10 @@
+export default {
+    name: "Mainfooter",
+    template: `
+        <footer class="footer">
+            <section>
+                <img class="logo" src="./LogoEchado.png" alt="DuckType">
+            </section>
+        </footer>
+    `
+}
---
+++ vue/vue-actividad3/Post.js
@@ -0,0 +1,8 @@
+export default {
+    name: "Post",
+    template: `
+        <article>
+            
+        </article>
+    `
+}
