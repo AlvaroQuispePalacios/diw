291ade039441bce2cf114d841a890ddc7cc8a601
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-02-23 17:26:09 +0100

    23-02-2024

--- vue/vue-actividad3/Post.js
+++ vue/vue-actividad3/Post.js
@@ -1,8 +1,21 @@
 export default {
     name: "Post",
+    props: ["post"],
     template: `
         <article>
+            <section>
+                Imagen
+                {{post.imagePost}}
+            </section>
+            <section>
+                <h2>{{post.titlePost}}</h2>
+                <p>{{post.contentPost}}</p>
+                <div>{{post.authorPost}}</div>
+                
+            </section>
             
+            {{post.creationDatePost}}
+
         </article>
     `
 }
