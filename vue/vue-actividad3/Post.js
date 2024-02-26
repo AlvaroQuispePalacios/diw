ee602a371b8066e816e0221f568431e55c0f7232
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-02-26 20:08:22 +0100

    26-02-2024

--- vue/vue-actividad3/Post.js
+++ vue/vue-actividad3/Post.js
@@ -1,21 +1,36 @@
 export default {
     name: "Post",
     props: ["post"],
+    methods: {
+        updatePost: function(index){
+            // Le dice al padre que algo pasa
+            this.$emit("updatePost", index);
+        },
+        deletePost: function(index){
+            this.$emit("deletePost", index);
+        }
+    },
     template: `
-        <article>
-            <section>
-                Imagen
-                {{post.imagePost}}
+        <article class="post-main">
+            <section class="post-img">
+                <img v-bind:src="post.imagePost" alt="imgPost">
             </section>
-            <section>
-                <h2>{{post.titlePost}}</h2>
-                <p>{{post.contentPost}}</p>
-                <div>{{post.authorPost}}</div>
-                
+            <section class="post-content">
+                <h2 class="post-content-header">{{post.titlePost}}</h2>
+                <p class="post-content-text">{{post.contentPost}}</p>
+                <div class="post-content-footer">
+                    <span>
+                        {{post.authorPost}}
+                    </span>
+                    <span>
+                        {{post.creationDatePost}}
+                    </span>
+                </div>
+                <div class="post-content-botones">
+                    <button @click="updatePost(index)">Actualizar</button>
+                    <button @click="deletePost(index)">Eliminar</button>
+                </div>
             </section>
-            
-            {{post.creationDatePost}}
-
         </article>
     `
 }
