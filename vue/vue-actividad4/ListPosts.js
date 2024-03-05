c3f35c4a0903f076c3383b9b2e43c4d850cd1889
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-03-05 18:56:46 +0100

    05-03-2024

--- vue/vue-actividad4/ListPosts.js
+++ vue/vue-actividad4/ListPosts.js
@@ -3,6 +3,9 @@ export default {
     name: "ListPosts",
     props: ["posts"],
     template: `
+        <router-link :to="{name: 'Formcreateeditpost', params: {btnCreatePost: true, btnSetPost: false} }" >
+            <button>Crear Post</button>
+        </router-link>
         <section class="posts">
             <Post v-for="(post, index) in posts" :post="post" :index="index"  @delete-post="deletePost(index)"></Post>
         </section>
