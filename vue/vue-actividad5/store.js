1a28f7898831997074d022b93995eb5faaf53fbc
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-03-12 19:40:08 +0100

    12-03-2024

--- vue/vue-actividad5/store.js
+++ vue/vue-actividad5/store.js
@@ -1,15 +1,23 @@
-const store = Pinia.defineStore('counter', {
-    state: () => ({ 
-    //   count: 0 
+const store = Pinia.defineStore("counter", {
+    state: () => ({
+        //   count: 0
+        posts: [""],
+        saludar: "Hola"
     }),
     getters: {
-    //   double: (state) => state.count * 2,
+        //   double: (state) => state.count * 2,
     },
     actions: {
-    //   increment() {
-    //     this.count++;
-    //   },
+
+        getPosts: function(){
+            if (localStorage.getItem("posts") != null) {
+                this.posts = JSON.parse(localStorage.getItem("posts"));
+            } else {
+                console.log("No existen post para mostrar");
+                this.posts = [];
+            }
+        }
     },
-  });
-  
-  export default store
+});
+
+export default store;
